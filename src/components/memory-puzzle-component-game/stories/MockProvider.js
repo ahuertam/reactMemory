94582545cb9b6dc70653865341106fcf2dad964d
Alejandro Huerta <ahuertam@yahoo.es>
2020-04-07 16:45:17 +0200

    fix url

--- src/components/memory-puzzle-component-game/stories/MockProvider.js
+++ src/components/memory-puzzle-component-game/stories/MockProvider.js
@@ -9,47 +9,47 @@ export const MockProvider = ({ children }) => {
     documentsDict: {
       '5e4d39175cd915000f27c6ea': {
         url:
-          'https://imgur.com/a7EDcwg',
+          'https://i.imgur.com/a7EDcwg.png',
       },
       '5e4d3a035cd915000f27c6ed': {
         url:
-          'https://imgur.com/W7knvV2',
+          'https://i.imgur.com/W7knvV2.png',
       },
       '5e4d3a145cd915000f27c6ee': {
         url:
-          'https://imgur.com/Lw9uGI0',
+          'https://i.imgur.com/Lw9uGI0.png',
       },
       '5e4d39c95cd915000f27c6eb': {
         url:
-          'https://imgur.com/yPCJ07F',
+          'https://i.imgur.com/yPCJ07F.png',
       },
       '5e4d39ea5cd915000f27c6ec': {
         url:
-          'https://imgur.com/DPMU60z',
+          'https://i.imgur.com/DPMU60z.png',
       },
       '5e4d3a395cd915000f27c6ef': {
         url:
-          'https://imgur.com/7zyk7bK',
+          'https://i.imgur.com/7zyk7bK.png',
       },
       '5e550ef0366c28000f563d79': {
         url:
-          'https://imgur.com/swnA1cb',
+          'https://i.imgur.com/swnA1cb.png',
       },
       '5e550f74366c28000f563d7a': {
         url:
-          'https://imgur.com/IxRFF0L',
+          'https://i.imgur.com/IxRFF0L.png',
       },
       '5e550f86366c28000f563d7b': {
         url:
-          'https://imgur.com/X7X3fel',
+          'https://i.imgur.com/X7X3fel.png',
       },
       '5e550f98f861f3000f303e9e': {
         url:
-          'https://imgur.com/eEGa5FM',
+          'https://i.imgur.com/eEGa5FM.png',
       },
       '5e550fabf861f3000f303e9f': {
         url:
-          'https://imgur.com/OxLqsBO',
+          'https://i.imgur.com/OxLqsBO.png',
       },
     },
     references: {
